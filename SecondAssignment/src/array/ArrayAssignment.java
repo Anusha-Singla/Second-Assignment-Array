5ea25e8beee671325d37c7f7bbae7b84ffcdc0fd
Anusha Singla <anushasingla737@gmail.com>
2021-05-30 23:37:09 +0530

    finding the first occurence of positive integer

--- SecondAssignment/src/array/ArrayAssignment.java
+++ SecondAssignment/src/array/ArrayAssignment.java
@@ -26,21 +26,51 @@ static int removeDuplicates(int a[], int n)
     return j;
 }
 
+static int firstOccPositve(int a[], int low, int high)
+{
+	if(high >= low)
+  {
+      int mid = (low + high)/2; /*low + (high - low)/2;*/
+      if(mid == a[mid]){
+          return mid;
+      }
+      
+      if(mid > a[mid]){
+           return firstOccPositve(a, (mid + 1), high);
+      }
+
+      else{
+          return firstOccPositve(a, low, (mid -1));
+      }
+  }
+  return -1;
+}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("Enter the size of array");
 		 int size = s.nextInt();
 		int[] a = new int[size+1];
  		for(int i=1;i<a.length;i++) {
- 			a[i] = s.nextInt();         //pushing the elements in array
+ 			a[i] = s.nextInt();                       //pushing the elements in array
  		}
  		Arrays.sort(a, 1,(a.length));     
- 		int size2 = removeDuplicates(a, (a.length));
+ 		int size2 = removeDuplicates(a, (a.length));  //removing duplicates
  		
-		System.out.println("Elements are: ");
+ 		int index = firstOccPositve(a,1, (size2-1));
+		
+		if (index != -1){
+	        System.out.println("first positive :"+index);
+	       }
+	    else{
+	    	System.out.println("not found");
+	    }
+ 		
+/*	System.out.println("Elements are: ");
  		for(int i=1;i<size2;i++){
  			System.out.println("Elements in " +i+ " Index " + a[i]);
- 		}
+	} */
+ 		
 	}
 
 }
